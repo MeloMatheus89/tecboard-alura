9fbcfa5300aeec4e45fdf155e7c1aada3a4db990
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2026-01-16 20:59:49 -0300

    feat: add input styles and fieldset component, import styles in form components

--- src/componentes/CampoDeFormulario/index.jsx
+++ src/componentes/CampoDeFormulario/index.jsx
@@ -1,3 +1,5 @@
+import "./fieldset.estilos.css";
+
 export function CampoDeFormulario({ children }) {
   return <fieldset>{children}</fieldset>;
 }
--- src/componentes/TituloFormulario/index.jsx
+++ src/componentes/TituloFormulario/index.jsx
@@ -1,3 +1,5 @@
+import "./h2.estilos.css";
+
 export function TituloFormulario(tituloFormulario) {
   return <h2>{tituloFormulario.children}</h2>;
 }
