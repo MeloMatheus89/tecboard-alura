2bc6d9c59abcfac97e8d6d58bcc0dd2eb2f4ade6
MeloMatheus89 <156861214+MeloMatheus89@users.noreply.github.com>
2026-01-21 00:19:06 -0300

    feat: enhance form components with new styles and structure, add selection field and button

---
+++ src/componentes/CampoDeSelecao/index.jsx
@@ -0,0 +1,11 @@
+import "./campo-de-selecao.estilos.css";
+
+export function CampoDeSelecao() {
+  return (
+    <select className="campo-de-selecao">
+      <option> </option>
+    </select>
+  );
+}
+
+// Usar um forEach??
--- src/componentes/FormularioDeEvento/index.jsx
+++ src/componentes/FormularioDeEvento/index.jsx
@@ -6,15 +6,30 @@ import { TituloFormulario } from "../TituloFormulario/index.jsx";
 import { CampoDeFormulario } from "../CampoDeFormulario/index.jsx";
 import { CampoDeEntrada } from "../CampoDeEntrada/index.jsx";
 import { Label } from "../Label/index.jsx";
+import { CampoDeSelecao } from "../CampoDeSelecao/index.jsx";
+import { Botao } from "../Botao/index.jsx";
 
 export function FormularioDeEvento() {
   return (
     <form className="form-evento">
       <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
-      <CampoDeFormulario>
-        <Label htmlFor="nome">Qual o nome do evento?</Label>
-        <CampoDeEntrada type="text " id="nome" placeholder="Summer dev hits"></CampoDeEntrada>
-      </CampoDeFormulario>
+      <div className="campos">
+        <CampoDeFormulario>
+          <Label htmlFor="nome">Qual o nome do evento?</Label>
+          <CampoDeEntrada type="text " id="nome" placeholder="Summer dev hits"></CampoDeEntrada>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="dataEvento">Data do evento</Label>
+          <CampoDeEntrada type="date" id="dataEvento" name="dataEvento"></CampoDeEntrada>
+        </CampoDeFormulario>
+        <CampoDeFormulario>
+          <Label htmlFor="temaEvento">Tema do evento </Label>
+          <CampoDeSelecao />
+        </CampoDeFormulario>
+      </div>
+      <div className="acoes">
+        <Botao>Criar evento</Botao>
+      </div>
     </form>
   );
 }
